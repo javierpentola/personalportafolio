6c6ab0be5676e860fbaabbff9a0b5a3aaf359f29
Javier Pentola <80289598+javierpentola@users.noreply.github.com>
2025-03-15 01:56:50 +0100

    Update useBannerContent.ts

--- hooks/useBannerContent.ts
+++ hooks/useBannerContent.ts
@@ -1,8 +1,10 @@
 import { useLanguage } from "@/contexts/LanguageContext"
 import { bannerContent } from "@/data/bannerContent"
 
+type LanguageKey = keyof typeof bannerContent
+
 export const useBannerContent = () => {
   const { language } = useLanguage()
-  return bannerContent[language] || bannerContent.en
+  return bannerContent[language as LanguageKey] || bannerContent.en
 }
 
